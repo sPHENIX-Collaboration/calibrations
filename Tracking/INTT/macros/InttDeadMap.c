133b17789d07619378770ae9aa5d9cfb614bb303
josephbertaux <jbertau@purdue.edu>
2023-05-04 19:49:26 -0400

    Implemented an analysis method that could be improved later but is simple, stable, and versatile for now

--- Tracking/INTT/macros/InttDeadMap.c
+++ Tracking/INTT/macros/InttDeadMap.c
@@ -4,12 +4,12 @@ void InttDeadMap()
 {
 	struct InttDeadMap_Param_s params;
 
-	InttDeadMap_Long_t N = 313100;
-	InttDeadMap_Double_t frac = 0.02;
+	InttDeadMap_Long_t N = 31310;
+	InttDeadMap_Double_t frac = 0.2;
 
         struct InttDeadMap_Point_s* list = new struct InttDeadMap_Point_s[N];
 
-        init_params(&params, list, N, 3.0, 3.0);
+        init_params(&params, list, N, 2.0, 2.0);
         gen_points(&params, frac);
 	//quicksort_hitkey(&params);
 	quicksort_counts(params.points, 0, N);
@@ -23,7 +23,7 @@ void InttDeadMap()
 	InttDeadMap_Double_t i_upper_ref = N - ceil(frac * N);
 
         std::cout << "\ti_lower: " << params.i_lower << "\t(" << i_lower_ref << ")\t\trel err: " << (params.i_lower - i_lower_ref) / i_lower_ref * 100.0 << "\%" << std::endl;
-        std::cout << "\ti_upper: " << params.i_upper << "\t(" << i_upper_ref << ")\trel err: " << (params.i_upper - i_upper_ref) / i_upper_ref * 100 << "\%" << std::endl;
+        std::cout << "\ti_upper: " << params.i_upper << "\t(" << i_upper_ref << ")\trel err: " << (params.i_upper - i_upper_ref) / i_lower_ref * 100 << "\%" << std::endl;
 
         delete[] list;
 }
